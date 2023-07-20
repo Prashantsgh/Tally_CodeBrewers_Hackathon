afa0d0abde032f46696d0572f55f7a9abecf4674
Chirag Kalwani <75169388+chirag-kalwani@users.noreply.github.com>
2023-07-20 21:38:47 +0530

    typing feature added

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let timer = document.getElementById('timer'); // select the input
 let counter = document.getElementById('counter'); // show the timer
-let text = document.getElementById('text'); // show the text
+let type = document.getElementById('type'); // show the text
+let showScore = document.getElementById('showScore');
 let selectedTimer = 60 // timer selected by user default is 60
 let startTimer = false; // true after starting time
 let id; // for setInterval
+let text = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"];
+let userText = [""];
 
 
 timer.onchange = function (e) {
@@ -13,6 +16,17 @@ timer.onchange = function (e) {
                         </div>
                         `
 };
+document.addEventListener("DOMContentLoaded", async function () {
+    type.innerHTML = `<div class="caret"></div>`
+    for (let i = 0; i < text.length; i++) {
+        for (let j = 0; j < text[i].length; j++) {
+            type.innerHTML += `<span class="word gray">
+                                ${text[i][j]}
+                        </span>`
+        }
+        type.innerHTML += `<span> &nbsp; </span>`
+    }
+});
 
 function start() {
     if (!startTimer) {
@@ -27,27 +41,71 @@ function start() {
         }, 1000);
 
         setTimeout(() => {
-            console.log("completed");
+            let score = 0, total = 0;
+            for (let i = 0; i < text.length; i++) {
+                total += text[i].length;
+            }
+            for (let i = 0; i < userText.length; i++) {
+                for (let j = 0; j < userText[i].length; j++) {
+                    if (j < text[i].length && userText[i][j] === text[i][j]) {
+                        score++;
+                    }
+                }
+            }
+            score = ((score / total) * 100).toString();
+            showScore.innerText = parseFloat(score).toFixed(2) + "%";
             clearInterval(id);
         }, stop * 1000);
     }
 }
 
-text.onkeyup = function (e) {
-    start(); // called only once
-}
+document.addEventListener('keydown', function (e) {
+    if (e.key >= 'a' && e.key <= 'z' || e.key === ' ' || e.key === 'Backspace') {
 
-
+        start(); // called only once
+        if (e.key === ' ') {
+            userText.push("");
+        } else if (e.key === 'Backspace') {
+            if (userText[userText.length - 1] !== "") {
+                userText[userText.length - 1] = userText[userText.length - 1].slice(0, -1);
+            }
+        } else
+            userText[userText.length - 1] += e.key;
+        type.innerHTML = ``
+        console.log(userText)
+        for (let i = 0; i < userText.length; i++) {
+            for (let j = 0; j < userText[i].length; j++) {
+                if (j < text[i].length && userText[i][j] === text[i][j]) {
+                    type.innerHTML += `<span class="word green">
+                                ${userText[i][j]}
+                        </span>`
+                } else {
+                    type.innerHTML += `<span class="word red">
+                                ${text[i][j] || userText[i][j]}
+                        </span>`
+                }
+            }
+            if (i === userText.length - 1)
+                type.innerHTML += `<span class="caret"></span>`
+            for (let j = userText[i].length; j < text[i].length; j++) {
+                type.innerHTML += `<span class="word ${i === userText.length - 1 ? "gray" : "red"}">
+                                ${text[i][j]}
+                        </span>`
+            }
+            type.innerHTML += `<span> &nbsp; </span>`
+        }
+        for (let i = userText.length; i < text.length; i++) {
+            for (let j = 0; j < text[i].length; j++) {
+                type.innerHTML += `<span class="word gray">
+                                ${text[i][j]}
+                        </span>`
+            }
+            type.innerHTML += `<span> &nbsp; </span>`
+        }
+    }
+})
 
 function restart() {
-    clearInterval(id);
-    startTimer = false;
-    timer.disabled = false;
-    selectedTimer = 60;
-    timer.value = 60;
-    counter.innerHTML = `<div class="number">
-                            <p>60s</p>
-                        </div>
-                        `;
+    location.reload();
 }
 
